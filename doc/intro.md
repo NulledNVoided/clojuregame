5ee9427931e02bb31625e555d6a2af6639f4dbc7
NulledNVoided <jonathan_h@ip-172-31-32-107.ec2.internal>
2017-08-02 21:23:55 +0000

    "new project"

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojuregame
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojuregame/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojuregame.core-test
+  (:require [clojure.test :refer :all]
+            [clojuregame.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
